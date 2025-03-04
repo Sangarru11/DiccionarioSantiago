81d0c479c604b5bfc5ec7e0b26f8a7c04474d482
Santiago <105152807+Sangarru11@users.noreply.github.com>
2025-03-04 09:54:42 +0100

    first commit

---
+++ src/main/java/com/github/sangarru11/diccionariosantiago/DiccionarioSantiagoApplication.java
@@ -0,0 +1,13 @@
+package com.github.sangarru11.diccionariosantiago;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiccionarioSantiagoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiccionarioSantiagoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/sangarru11/diccionariosantiago/DiccionarioSantiagoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.sangarru11.diccionariosantiago;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiccionarioSantiagoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
